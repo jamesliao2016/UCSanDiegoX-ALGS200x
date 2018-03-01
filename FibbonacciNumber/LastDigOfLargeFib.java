c2ece0d84a5630292044e00534601834cc822374
Meg <the_meej@yahoo.com>
2018-03-01 13:48:17 -0500

    combo of addition and use of golden ratio

--- FibbonacciNumber/LastDigOfLargeFib.java
+++ FibbonacciNumber/LastDigOfLargeFib.java
@@ -4,34 +4,35 @@ import java.math.BigInteger;
 import java.util.Scanner;
 
 public class LastDigOfLargeFib {
-    static final double sqrtFive = Math.sqrt(5);
-    // Golden ratio numbers, used mathematically to evaluate n-th fibonacci
-    static final double bigPhi = 1.6180339;
-    static final double littlePhi = -1 / bigPhi;
 
     private static BigInteger calc_fib(int n) {
+        final double sqrtFive = Math.sqrt(5);
+        // Golden ratio numbers, used mathematically to evaluate n-th fibonacci
+        final double bigPhi = 1.6180339;
+        final double littlePhi = -1 / bigPhi;
+        BigInteger theFib = BigInteger.valueOf(0);
         if (n <= 1) {
             return BigInteger.valueOf(n);
-        }
-
-        BigInteger theFib = BigInteger.ZERO;
-
+        } else if (n > 1 && n <= 28) {
         // golden ratio works up to 28
-        if (n > 1 || n <= 28) {
             // must round up for it to be correct
             theFib = BigInteger.valueOf(Math.round((Math.pow(bigPhi, n) - Math.pow(littlePhi, n)) / sqrtFive));
             return theFib.mod(BigInteger.valueOf(10));
-        }
+        } else { //if (n > 28)
+            // Calculate the 27 & 28th Fib num, then continue on calculating by addition
+            BigInteger prev2 = BigInteger.valueOf(Math.round((Math.pow(bigPhi, 27) - Math.pow(littlePhi, 27)) / sqrtFive));
+            System.out.println("prev2/f(27) = " + prev2);
+            BigInteger prev = BigInteger.valueOf(Math.round((Math.pow(bigPhi, 28) - Math.pow(littlePhi, 28)) / sqrtFive));
+            System.out.println("prev/f(28) = " + prev);
 
-        // Calculate the 28th Fib num, then continue on calculating the standard way
-        BigInteger prev2 = BigInteger.valueOf(Math.round((Math.pow(bigPhi, 27) - Math.pow(littlePhi, 27)) / sqrtFive));
-        BigInteger prev = BigInteger.valueOf(Math.round((Math.pow(bigPhi, 28) - Math.pow(littlePhi, 28)) / sqrtFive));
-        for (int i = 29; i < n; i++) {
-            theFib = prev2.add(prev);
-            prev2 = prev;
-            prev = theFib;
+            for (int i = 28; i < n; i++) {
+                theFib = prev2.add(prev);
+                prev2 = prev;
+                prev = theFib;
+            }
+            System.out.println(theFib);
+            return theFib.mod(BigInteger.valueOf(10));
         }
-        return theFib.mod(BigInteger.valueOf(10));
     }
 
     public static void main(String args[]) {
