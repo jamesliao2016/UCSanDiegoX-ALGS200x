ab4c765265caa7519bf88bd8c5a65c52b237e778
Meg <the_meej@yahoo.com>
2018-03-01 14:05:01 -0500

    uses last digit only earlier to save time

--- FibbonacciNumber/LastDigOfLargeFib.java
+++ FibbonacciNumber/LastDigOfLargeFib.java
@@ -14,24 +14,23 @@ public class LastDigOfLargeFib {
         if (n <= 1) {
             return BigInteger.valueOf(n);
         } else if (n > 1 && n <= 28) {
-        // golden ratio works up to 28
+            // golden ratio works up to 28
             // must round up for it to be correct
             theFib = BigInteger.valueOf(Math.round((Math.pow(bigPhi, n) - Math.pow(littlePhi, n)) / sqrtFive));
             return theFib.mod(BigInteger.valueOf(10));
         } else { //if (n > 28)
             // Calculate the 27 & 28th Fib num, then continue on calculating by addition
+            // use only the last digit from the beginning
             BigInteger prev2 = BigInteger.valueOf(Math.round((Math.pow(bigPhi, 27) - Math.pow(littlePhi, 27)) / sqrtFive));
-            System.out.println("prev2/f(27) = " + prev2);
+            prev2 = prev2.mod(BigInteger.valueOf(10));
             BigInteger prev = BigInteger.valueOf(Math.round((Math.pow(bigPhi, 28) - Math.pow(littlePhi, 28)) / sqrtFive));
-            System.out.println("prev/f(28) = " + prev);
-
+            prev = prev.mod(BigInteger.valueOf(10));
             for (int i = 28; i < n; i++) {
-                theFib = prev2.add(prev);
+                theFib = prev2.add(prev).mod(BigInteger.valueOf(10));
                 prev2 = prev;
                 prev = theFib;
             }
-            System.out.println(theFib);
-            return theFib.mod(BigInteger.valueOf(10));
+            return theFib;
         }
     }
 
