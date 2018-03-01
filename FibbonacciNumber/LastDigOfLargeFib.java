4e2c30c09046ffa4b5b5e1aa474b23f5c6a59af2
Meg <the_meej@yahoo.com>
2018-03-01 12:30:09 -0500

    2nd attempt using golden ratio

--- FibbonacciNumber/LastDigOfLargeFib.java
+++ FibbonacciNumber/LastDigOfLargeFib.java
@@ -4,18 +4,33 @@ import java.math.BigInteger;
 import java.util.Scanner;
 
 public class LastDigOfLargeFib {
-    static double sqrtFive = Math.sqrt(5);
+    static final double sqrtFive = Math.sqrt(5);
+    // Golden ratio numbers, used mathematically to evaluate n-th fibonacci
+    static final double bigPhi = 1.6180339;
+    static final double littlePhi = -1 / bigPhi;
 
     private static BigInteger calc_fib(int n) {
         if (n <= 1) {
             return BigInteger.valueOf(n);
         }
-        // Approx value of golden ratio, used mathematically to evaluate n-th fibonacci
-        double bigPhi = 1.6180339;
-        double littlePhi = -1/bigPhi;
-        // must round up for it to be correct
-        BigInteger theFib = BigInteger.valueOf(Math.round((Math.pow(bigPhi, n) - Math.pow(littlePhi, n)) / sqrtFive));
 
+        BigInteger theFib = BigInteger.ZERO;
+
+        // golden ratio works up to 28
+        if (n > 1 || n <= 28) {
+            // must round up for it to be correct
+            theFib = BigInteger.valueOf(Math.round((Math.pow(bigPhi, n) - Math.pow(littlePhi, n)) / sqrtFive));
+            return theFib.mod(BigInteger.valueOf(10));
+        }
+
+        // Calculate the 28th Fib num, then continue on calculating the standard way
+        BigInteger prev2 = BigInteger.valueOf(Math.round((Math.pow(bigPhi, 27) - Math.pow(littlePhi, 27)) / sqrtFive));
+        BigInteger prev = BigInteger.valueOf(Math.round((Math.pow(bigPhi, 28) - Math.pow(littlePhi, 28)) / sqrtFive));
+        for (int i = 29; i < n; i++) {
+            theFib = prev2.add(prev);
+            prev2 = prev;
+            prev = theFib;
+        }
         return theFib.mod(BigInteger.valueOf(10));
     }
 
